35824b9b7ebe6ace66b03dae78c740b32e6938a3
Shashank nath <shashankocean@gmail.com>
2021-03-05 03:06:55 +0530

    Provided comments

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 from bs4 import BeautifulSoup
 from openpyxl import Workbook
-import aiohttp
 import requests
-import asyncio
-import json
-
-
-# Press the green button in the gutter to run the script.
 
 
 def get_product_information(url):
@@ -15,9 +9,12 @@ def get_product_information(url):
 
 
 if __name__ == '__main__':
+    # Collecting list of mens air shoes
     nike_web_new_arr = requests.session().get("https://www.nike.com/in/w/mens-air-force-1-shoes-5sj3yznik1zy7ok")
     test = BeautifulSoup(nike_web_new_arr.content, 'html.parser')
     new_arrival = test.find_all(class_='product-card')
+
+    # Creating table in excel
     excel_file = Workbook()
     sheet_new_arrival = excel_file.active
     sheet_new_arrival.title = 'New Arrival Shoes'
@@ -28,6 +25,7 @@ if __name__ == '__main__':
     sheet_new_arrival['E1'] = 'Colors'
     sheet_new_arrival['F1'] = 'Product Link'
 
+    # collecting shoes information like name, price, discount price and short information
     for n in range(0, len(new_arrival)):
         product_link_ = new_arrival[n].find('figure').find('a').get('href')
         name_ = new_arrival[n].find('figure').find(class_='product-card__title').text
@@ -43,6 +41,7 @@ if __name__ == '__main__':
         sheet_new_arrival['D{}'.format(n + 2)] = discount_price_
         sheet_new_arrival['F{}'.format(n + 2)] = product_link_
 
+        # Colour information is not present in listing page hence Opening each shoe's detail page for color information.
         html = get_product_information(product_link_)
         colours_ = []
         any_color = BeautifulSoup(html.content, 'html.parser').find(class_='colorway-images-wrapper')
@@ -56,6 +55,8 @@ if __name__ == '__main__':
                 class_='description-preview__color-description')
             print('One Color: {}'.format(single_color))
             colours_.append(single_color.text) if single_color else ''
+
+        # finally storing colour information in table as well
         sheet_new_arrival['E{}'.format(n + 2)] = '|'.join(colours_)
 
     excel_file.save('Nike.xlsx')
